e76cf5565aadb02d8e6108b340596fa6cad04533
Richard Torruellas <rtorruellas@gmail.com>
2019-05-11 10:07:51 -0700

    removing bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,6 @@ class BlogIndex extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" keywords={[`javascript`, `react`]} />
-        <Bio />
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
           return (
